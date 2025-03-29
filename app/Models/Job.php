f63d05b851f8c7161d51f9bae7d4ae0d8385b43b
thomsjel <it221506@fhstp.ac.at>
2025-03-29 13:52:25 +0100

    add episode 09: meet eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,40 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job{
+class Job extends Model{
 
-    public static function all(): array{
-        return [
-            [
-                'id' => 1,
-                'title' => 'PHP Developer',
-                'salary' => '$60,000',
-                'tags' => ['PHP', 'Remote']
-            ],
-            [
-                'id' => 2,
-                'title' => 'Python Developer',
-                'salary' => '$70,000',
-                'tags' => ['Python', 'Remote']
-            ],
-            [
-                'id' => 3,
-                'title' => 'React Developer',
-                'salary' => '$80,000',
-                'tags' => ['React', 'Remote']
-            ]
-        ];
-    }
-
-    public static function find(int $id): array{
-        $job = Arr::first(self::all(), fn($job) => $job['id'] == $id);
-
-        if(!$job){
-            abort(404);
-        }else{
-            return $job;
-        }
-    }
+    protected $table = 'job_listings';
+    protected $fillable = [
+        'title',
+        'salary',
+        'tags'
+    ];
 }
--- database/migrations/2025_03_29_124546_create_comments_table.php
+++ database/migrations/2025_03_29_124546_create_comments_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('job_listings', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('salary');
-            $table->json('tags')->nullable();
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_listings');
+        Schema::dropIfExists('comments');
     }
 };
